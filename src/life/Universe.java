a6642592f767fe8d1ad331df197502f9ecbc02ba
Chelsea Hilditch <git@cdhildit.ch>
2020-09-01 20:32:31 +0100

    Remove unused functions and variables

--- src/life/Universe.java
+++ src/life/Universe.java
@@ -2,19 +2,11 @@ package life;
 
 public class Universe {
     public Map currentGeneration;
-    public int numberOfGenerations;
     private int generationNumber;
 
     // Initialises a universe with a randomly generated map
     public Universe(int size) {
         this.currentGeneration = new Map(size, false);
-        this.generationNumber = 1;
-    }
-
-    // Initialises a universe with a randomly generated map and a maximum number of generations
-    public Universe(int size, int numberOfGenerations) {
-        this.currentGeneration = new Map(size, false);
-        this.numberOfGenerations = numberOfGenerations;
         this.generationNumber = 1;
     }
 
@@ -26,11 +18,5 @@ public class Universe {
     public void advance() {
         this.currentGeneration = Generation.evolve(currentGeneration);
         this.generationNumber++;
-    }
-
-    public void run() {
-        while (this.generationNumber < this.numberOfGenerations) {
-            advance();
-        }
     }
 }
